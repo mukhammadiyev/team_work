6bea1a7266e5a05f1f834f04bf53b12cdabcc39c
mukhammadiyev <muhammadsolihmuhammadiyev2009@gmail.com>
2025-07-02 21:59:11 +0500

    Phone version updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
 import './Footer.css'
 function Footer() {
-	return <div>Footer</div>
+	return (
+		<footer>
+			<div className='container'>
+				<div className='footer'>
+					<h1 className='footer_logo'>Mini It Blog</h1>
+					<p>
+						All rights reserved <a href='#'>IT New Kelajak</a>'s first team work
+					</p>
+					<div className='socials'>
+						<i className='fa-brands fa-telegram'></i>
+						<i className='fa-brands fa-github'></i>
+						<i className='fa-brands fa-linkedin'></i>
+					</div>
+				</div>
+			</div>
+		</footer>
+	)
 }
 
 export default Footer
